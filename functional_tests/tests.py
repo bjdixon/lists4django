efaa1b224c4c65c5e2ea1d802abbbcc1a74ecc34
bjdixon <bjdixon@gmail.com>
2014-01-20 13:26:58 -0500

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,3 +1,4 @@
+import sys
 from selenium import webdriver
 from django.test import LiveServerTestCase
 from selenium.webdriver.common.keys import Keys
@@ -5,21 +6,41 @@ import unittest
 
 class NewVisitorTest(LiveServerTestCase):
 
+
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url = 'http://' + arg.split('=')[1]
+				return
+		LiveServerTestCase.setUpClass()
+		cls.server_url = cls.live_server_url
+
+
+	@classmethod
+	def tearDownClass(cls):
+		if cls.server_url == cls.live_server_url:
+			LiveServerTestCase.tearDownClass()
+
+
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
 
+
 	def tearDown(self):
 		self.browser.quit()
+
 
 	def check_for_row_in_list_table(self, row_text):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertIn(row_text, [row.text for row in rows])
 
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# User (Edith) visits homepage
-		self.browser.get(self.live_server_url)	
+		self.browser.get(self.server_url)	
 
 		# she notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
@@ -61,7 +82,7 @@ class NewVisitorTest(LiveServerTestCase):
 		self.browser = webdriver.Firefox()
 
 		# Francis visits the home page. There is no sign of Ediths list
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertNotIn('make a fly', page_text)
@@ -83,9 +104,10 @@ class NewVisitorTest(LiveServerTestCase):
 		
 		# The end
 
+
 	def test_layout_and_styleing(self):
 		# Edith goes to the home page
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.browser.set_window_size(1024, 768)
 
 		# She notices the input box is nicely centered
