536e42b9e6b6dab61d1d5be154720e4e12811376
bjdixon <bjdixon@gmail.com>
2014-02-08 14:52:02 -0500

    Use form in all views, back to working state

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -5,7 +5,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from lists.models import Item, List
 from django.utils.html import escape
-from lists.forms import ItemForm
+from lists.forms import ItemForm, EMPTY_LIST_ERROR
 
 
 class HomePageTest(TestCase): 
@@ -106,6 +106,13 @@ class ListViewTest(TestCase):
 		self.assertContains(response, expected_error)
 
 
+	def test_displays_item_form(self):
+		list_ = List.objects.create()
+		response = self.client.get('/lists/%d/' % (list_.id,))
+		self.assertIsInstance(response.context['form'], ItemForm)
+		self.assertContains(response, 'name="text"')
+
+
 class NewListTest(TestCase):
 
 
@@ -128,10 +135,28 @@ class NewListTest(TestCase):
 		self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
 
 
-	def test_validation_errors_sent_back_to_home_page_template(self):
-		response = self.client.post('/lists/new', data={'text': ''})
-		self.assertEqual(Item.objects.all().count(), 0)
-		self.assertTemplateUsed(response, 'home.html')
-		expected_error = escape("You can't have an empty list item")
-		self.assertContains(response, expected_error)
+	def post_invalid_input(self):
+		list_ = List.objects.create()
+		return self.client.post(
+			'/lists/%d/' % (list_.id,),
+			data={'text': ''}
+		)
+	
 
+	def test_invalid_input_means_nothin_saved_to_db(self):
+		self.post_invalid_input()
+		self.assertEqual(Item.objects.all().count(), 0)
+
+	
+	def test_invalid_input_renders_list_template(self):
+		response = self.post_invalid_input()
+		self.assertTemplateUsed(response, 'list.html')
+
+
+	def test_invalid_input_renders_form_with_errors(self):
+		response = self.post_invalid_input()
+		self.assertIsInstance(response.context['form'], ItemForm)
+		self.assertContains(response, escape(EMPTY_LIST_ERROR))
+
+	
+	
