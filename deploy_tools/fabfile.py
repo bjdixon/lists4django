e78b555ff0efc002cf3e70e63067d582904d3a81
bjdixon <bjdixon@gmail.com>
2014-02-08 20:23:04 -0500

    deploy script now does syncdb --migrate

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -54,12 +54,6 @@ def _update_static_files(source_folder):
 	))
 
 def _update_database(source_folder):
-	run('cd %s && ../virtualenv/bin/python3 manage.py syncdb' % (
-		source_folder,
-	))
-	run('cd %s && ../virtualenv/bin/python3 manage.py migrate lists --fake 0001' % (
-		source_folder,
-	))
-	run('cd %s && ../virtualenv/bin/python3 manage.py migrate' % (source_folder,))
+	run('cd %s && ../virtualenv/bin/python3 manage.py syncdb --migrate --noinput' % (source_folder,))
 
 
