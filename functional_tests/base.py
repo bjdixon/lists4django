ee6295c1a6607c70bfdf3397f599374db23b33d0
Brian J Dixon <bjdixon@gmail.com>
2014-03-24 18:10:36 -0400

    New FT for sharing, move session creation stuff to base

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -2,9 +2,9 @@ import sys
 from selenium import webdriver
 from django.test import LiveServerTestCase
 from selenium.webdriver.support.ui import WebDriverWait
-
-from .server_tools import reset_database
-
+from django.conf import settings
+from .server_tools import create_session_on_server, reset_database
+from .management.commands.create_session import create_pre_authenticated_session
 
 class FunctionalTest(LiveServerTestCase):
 
@@ -63,3 +63,18 @@ class FunctionalTest(LiveServerTestCase):
 		self.wait_for_element_with_id('id_login')
 		navbar = self.browser.find_element_by_css_selector('.navbar')
 		self.assertNotIn(email, navbar.text)
+
+	def create_pre_authenticated_session(self, email):
+		if self.against_staging:
+			session_key = create_session_on_server(self.server_host, email)
+		else:
+			session_key = create_pre_authenticated_session(email)
+		## to get a cookie we need to visit the domain
+		## 404 pages load the quickest
+		self.browser.get(self.server_url + "/404_no_such_url/")
+		self.browser.add_cookie(dict(
+			name=settings.SESSION_COOKIE_NAME,
+			value=session_key,
+			path='/',
+		))
+
--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -46,18 +46,3 @@ class MyListsTest(FunctionalTest):
 			[]
 		)
 
-
-	def create_pre_authenticated_session(self, email):
-		if self.against_staging:
-			session_key = create_session_on_server(self.server_host, email)
-		else:
-			session_key = create_pre_authenticated_session(email)
-		## to get a cookie we need to visit the domain
-		## 404 pages load the quickest
-		self.browser.get(self.server_url + "/404_no_such_url/")
-		self.browser.add_cookie(dict(
-			name=settings.SESSION_COOKIE_NAME,
-			value=session_key,
-			path='/',
-		))
-
